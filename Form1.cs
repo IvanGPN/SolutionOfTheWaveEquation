83e0676c600fad2d7744e18f2c6fbc4b8ce5905a
IvanGPN <gepner99@yandex.ru>
2020-12-01 16:07:25 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Явный_шаблок_крестик
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            double[,] result = HandlerClass.Calculate();
+
+            double h = 0;
+
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                chart1.Series.Add(Convert.ToString(i));
+                chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                for (int j = 0; j< 11; j++)
+                {
+                    chart1.Series[i].Points.AddXY(h, result[i,j]);
+                    h += 0.1;
+                    textBox1.Text += "\t" + result[i, j];
+                }
+                h = 0;
+                textBox1.Text += "\r\n";
+            }
+        }
+    }
+}
---
+++ HandlerClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Явный_шаблок_крестик
+{
+    public class HandlerClass
+    {
+        static public double[,] Calculate()
+        {
+            double s = 0.1;
+            double h = 0.1;
+            double q = 1;
+            double a = 1;
+
+            double[,] array = new double[10, 11];
+
+            for (int i = 0; i < 10; i++)
+                array[i, 0] = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (i == 0)
+                    {
+                        array[i, j] = 0.2 * (1 - s) * Math.Sin(Math.PI * s);
+                        s += 0.1;
+                    }
+                    else if (i == 1)
+                    {
+                        s = 0.1;
+                        array[i, j] = array[i - 1, j] + s * 0;
+                    }
+                    else if (i > 1)
+                    {
+                        array[i, j] = q * (array[i - 1, j - 1] - 2 * array[i - 1, j] + array[i - 1, j + 1]) + 2 * array[i - 1, j] - array[i - 2, j];
+                    }
+                    
+                }
+            }
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 1; j < 10; j++)
+            //    {
+            //        if (j + i > 10)
+            //            array[i, j] += 1;
+            //    }
+            //}
+                    
+
+            for (int i = 0; i < 10; i++)
+                array[i, 10] = 0;
+
+            return array;
+        }
+    }
+}
